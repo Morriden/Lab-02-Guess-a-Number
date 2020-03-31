f1b9b4c942871b9be83933566c1cdeaa7c2cbc8b
Erik Ford <ekford18@gmail.com>
2020-03-31 16:42:59 -0700

    Adding, win lose statements with disabled button

---
+++ app.js
@@ -0,0 +1,46 @@
+import compareNumbers from './javafunction/comparenumbersfunction.js';
+
+//an input of type of number for specifying the guess
+const guess = document.getElementById('user-guess');
+//a button to submit the guess
+const bombButton = document.getElementById('button');
+//display for the number of tries remaining
+const attemptsLeft = document.getElementById('attempts-left');
+//display of guess to high or too low
+const highOrLowSpan = document.getElementById('high-or-low');
+//display winorlose
+const winOrLose = document.getElementById('win-or-lose');
+
+
+
+let correctNumber = Math.ceil(Math.random() * 20);
+console.log(correctNumber);
+
+let remainCount = 4;
+
+const defuseBomb = () => {
+    let userGuess = Number(guess.value);
+    const guessResult = compareNumbers(userGuess, correctNumber);
+    console.log(userGuess);
+    if (guessResult === -1) {
+        remainCount--;
+        attemptsLeft.textContent = remainCount;
+        highOrLowSpan.textContent = 'Your Guess was to low!';
+        if (remainCount === 0) {
+            document.getElementById('button').disabled = true;
+        }
+        //highOrLowSpan.textContent = 'Your guess is to low'
+    } else if (guessResult === 0) {
+        winOrLose.textContent = 'You defused the bomb!!!';
+        //disable game play and winning message
+    } else {
+        remainCount--;
+        attemptsLeft.textContent = remainCount;
+        highOrLowSpan.textContent = 'Your guess was to high!';
+        if (remainCount === 0) {
+            document.getElementById('button').disabled = true;
+        }
+    }
+};
+
+bombButton.addEventListener('click', defuseBomb);
---
+++ javafunction/comparenumbersfunction.js
@@ -0,0 +1,15 @@
+function compareNumbers(guess, correctNumber) {
+    if (guess === correctNumber) {
+        return 0;
+    }
+
+    if (guess > correctNumber) {
+        return 1;
+    }
+
+    if (guess < correctNumber) {
+        return -1;
+    }
+}
+
+export default compareNumbers;
